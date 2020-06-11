52b582ec34e869d09c4cd9375584add0a03842a0
Steven Torres <stevetorres7562@gmail.com>
2020-06-10 20:19:52 -0400

    added in roll functionality with IBAction

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -24,5 +24,13 @@ class ViewController: UIViewController {
         //sets the first dice to half trasnparency
         //diceImageView1.alpha = 0.50
     }
+    
+    // Interface-Builder-Action
+    // IBAction describes a block of code to do something
+    // upon a specific stimulus
+    @IBAction func rollButton(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
 }
 
